e3c1814b0bc62dfc8fa894040be49a92af2b00d0
Agusienka <101678267+Agusienka@users.noreply.github.com>
2022-08-17 13:36:11 -0400

    done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React, { useState } from 'react'
 function App(){
     let [ counter, changeCounter ] = useState(1)
     window.changeCounter = changeCounter
+    const handleClick = () => {
+      changeCounter(counter + 1)}
+
     return (
       <div> 
         <h1>{counter}</h1>
